8f979ba38863d71305f47d27c1609c2669ba5d3b
reypjbaliguat <reypjbaliguat@gmail.com>
2025-02-22 16:40:39 +0800

    fix(context): token updated

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -5,7 +5,8 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const context = async ({ req }) => {
-    const token = req.headers.authorization.split(' ')[1] || '';
+    const token =
+        (req.headers && req.headers.authorization.split(' ')[1]) || '';
     try {
         const { id } = jwt.verify(token, JWT_SECRET);
         return { userId: id };
